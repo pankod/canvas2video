b943ebe2c4eea2888f0ac3c807a7047a00e5620c
Ali Emir Şen <senaliemir@gmail.com>
2020-08-28 15:48:48 +0300

    Initial PR (#1)
    * add packages and tsconfig
    
    * add prettier config and types for fabric, fluent-ffmpeg
    
    Co-authored-by: Necati Özmen <necati.ozmen.9@gmail.com>
    
    * add types and rewrite
    
    Co-authored-by: Necati Özmen <necati.ozmen.9@gmail.com>
    
    * import fixes and progress bar
    
    * gather types to a single file
    
    Co-authored-by: Necati Özmen <necati.ozmen.9@gmail.com>
    
    * simple error handling, recursive path check, silent option and returning stream from encoder
    
    * directory error handling, stream output fix
    
    * encoder tests and type checking
    
    * add types to build
    
    Co-authored-by: Necati Özmen <necati.ozmen.9@gmail.com>
    
    * gitignore test output, syntax fix on tests
    
    * add type error handling to renderer
    
    * fix progress bar
    
    * encoder background video overlay fix
    
    * encoder output to stream
    
    * add [@pankod/puulr] to logs
    
    * move type packages to dev dep.
    
    * add examples
    
    * example fix dev dependencies
    
    * update package lock
    
    * add readme headings
    
    * Add readme
    Co-authored-by: Ali Emir Şen <senaliemir@gmail.com>
    
    * Add cli gif
    Co-authored-by: Ali Emir Şen <senaliemir@gmail.com>
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * add push workflow
    
    * update push workflow
    
    * update test reporter id for codeclimate on push workflow
    
    * change package name and improve code coverage
    
    Co-authored-by: Necati Özmen <necati.ozmen.9@gmail.com>
    
    Co-authored-by: Necati Özmen <necati.ozmen.9@gmail.com>
    Co-authored-by: Omer Faruk APLAK <omeraplak@gmail.com>

---
+++ examples/src/hello-world.ts
@@ -0,0 +1,44 @@
+import { renderer, encoder } from "@pankod/canvas2video";
+import { Power3 } from "gsap";
+
+const helloWorld = async () => {
+    try {
+        const stream = await renderer({
+            silent: false,
+            width: 1920,
+            height: 1080,
+            fps: 30,
+            makeScene: (fabric, canvas, anim, compose) => {
+                var text = new fabric.Text("Hello world", {
+                    left: 400,
+                    top: 400,
+                    fontSize: 100,
+                    fill: "#f99339",
+                    angle: 0,
+                });
+                canvas.add(text);
+                anim.to(text, {
+                    duration: 1,
+                    angle: 360,
+                    ease: Power3.easeOut,
+                });
+                compose();
+            },
+        });
+
+        const output = await encoder({
+            silent: false,
+            frameStream: stream,
+            output: "output/hello-world.mp4",
+            fps: {
+                input: 30,
+                output: 30,
+            },
+        });
+        console.log("process done,", output.path);
+    } catch (e) {
+        console.log("error", e);
+    }
+};
+
+helloWorld();
---
+++ src/types.ts
@@ -0,0 +1,44 @@
+import { Readable, Writable } from "stream";
+import { TimelineMax } from "gsap";
+import fabric from "fabric/fabric-impl";
+
+type mediaPath = string;
+
+interface BaseConfig {
+    silent?: boolean;
+}
+
+interface EncoderConfig extends BaseConfig {
+    frameStream: Readable;
+    output: mediaPath;
+    backgroundVideo?: {
+        videoPath: mediaPath;
+        inSeconds: number;
+        outSeconds: number;
+    };
+    fps: {
+        input: number;
+        output: number;
+    };
+}
+interface EncoderOutput {
+    path: mediaPath;
+    stream: Writable;
+}
+
+type makeSceneFunction = (
+    fabricInstance: typeof fabric,
+    canvas: fabric.StaticCanvas,
+    anim: TimelineMax,
+    compose: () => void,
+) => void;
+
+interface RendererConfig extends BaseConfig {
+    width: number;
+    height: number;
+    fps: number;
+    makeScene: makeSceneFunction;
+}
+
+export type Encoder = (config: EncoderConfig) => Promise<EncoderOutput>;
+export type Renderer = (config: RendererConfig) => Promise<Readable>;
